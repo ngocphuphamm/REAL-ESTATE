b05e5110a4133536bb397b2d6b9b4b0ae449f954
longshaww <at400123@gmail.com>
2022-11-06 01:59:16 +0700

    input name add

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,17 +8,17 @@ module.exports = {
 		res.render("auth/register");
 	},
 	register: async (req, res) => {
-		const { Name, Username, Email, Password, Phone, Avatar } = req.body;
+		const { name, username, email, password, phone, avatar } = req.body;
 		try {
 			const newUser = await sequelize.query(
 				"CALL sp_Register (:pr_username, :pr_password, :pr_name, :pr_email, :pr_phone, :pr_avatar)",
 				{
 					replacements: {
-						pr_username: Username,
-						pr_password: Password,
-						pr_name: Name,
-						pr_email: Email,
-						pr_phone: Phone,
+						pr_username: name,
+						pr_password: username,
+						pr_name: email,
+						pr_email: password,
+						pr_phone: phone,
 						pr_avatar: "",
 					},
 				}
