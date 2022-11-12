d1a9a6ff6ad72d693b47e660ca054c852e703329
ngocphuphamm <ngocphupham682001@gmail.com>
2022-11-13 03:28:54 +0700

    update sql

--- database/filenotsuccessfull/postBai/postbai-1.sql
+++ database/filenotsuccessfull/postBai/postbai-1.sql
@@ -69,12 +69,10 @@ END; $$
 DELIMITER $$
 DROP PROCEDURE IF EXISTS sp_postFeed $$
 CREATE PROCEDURE sp_postFeed(pr_categoryid varchar(200), pr_title varchar(200), pr_description text
-							, pr_displayDistrict text, pr_price decimal(15,2), pr_area varchar(100)
-                            , pr_phone varchar(20),pr_address varchar(150), pr_lat double, pr_lng double, pr_userid varchar(200)
+					        , pr_price decimal(15,2), pr_area varchar(100)
+                            , pr_phone varchar(20),pr_address varchar(150), pr_userid varchar(200)
                             , pr_projectid char(20), pr_streetid int, pr_wardid int, pr_districtid int
-                            , pr_provinceid int, pr_bedroom int, pr_bathroom int, pr_floor int
-                            , pr_direction text, pr_balconyDirection text, pr_furniture text
-                            , pr_fontageArea int) 
+                            , pr_provinceid int) 
 BEGIN	
     DECLARE isExists INT DEFAULT -1;
         
@@ -82,6 +80,7 @@ BEGIN
 	
     DECLARE isCheckUserID INT DEFAULT -1;
    
+	DECLARE id_post char(40);
     
     SELECT COUNT(*) INTO isExists FROM categories c WHERE c.categoryid = pr_categoryid;
     
@@ -93,15 +92,26 @@ BEGIN
 	ELSEIF(isCheckAddress > 0) THEN
 		SELECT "Địa Chỉ Không Hợp Lệ";
 	ELSE
-		INSERT INTO posts (reid, categoryid, title, description, displayDistrict, price, area, phone, address, lat, lng, userid, projectid, streetid, wardid, districtid, provinceid, bedroom, bathroom, floor, direction, balconyDirection, furniture, fontageArea)
-		  VALUES (uuid(), pr_categoryid, pr_title, pr_description
-				, pr_displayDistrict, pr_price
-                , pr_area, pr_phone, pr_address 
-                , pr_lat, pr_lng,pr_userid
-                , pr_projectid, pr_streetid, pr_wardid, pr_districtid
-                , pr_provinceid, pr_bedroom, pr_bathroom, pr_floor, pr_direction
-                , pr_balconyDirection, pr_furniture, pr_fontageArea);
-		SELECT 1;
+		set id_post = uuid();
+        SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+        START TRANSACTION;
+			INSERT INTO posts (reid, categoryid, title, description, price
+								, area, phone, address, userid, projectid
+                                , streetid, wardid, districtid, provinceid)
+			VALUES (id_post, pr_categoryid, pr_title, pr_description
+					 , pr_price , pr_area, pr_phone, pr_address ,pr_userid
+					, pr_projectid, pr_streetid, pr_wardid, pr_districtid
+					, pr_provinceid);
+		COMMIT;
+
+
+        SELECT id_post;
 	END IF; 
 END; $$
+select @@transaction_isolation;
+
+
+-- , pr_bedroom, pr_bathroom, pr_floor, pr_direction
+-- 					, pr_balconyDirection, pr_furniture, pr_fontageArea
+
 
